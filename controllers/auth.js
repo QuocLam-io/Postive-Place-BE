ade429e8598eeabf4234dd485b8d60af27e6b793
frostycakes <lamquoc.24@gmail.com>
2022-07-07 08:37:14 -0400

    Routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,33 +2,35 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
-//models
+/* --------------------------------- Models --------------------------------- */
 const User = require("../models/User");
 
-//Signup
-router.post("/signup", (req, res) =>{
-
+/* ------------------------------ Signup Route ------------------------------ */
+router.post("/signup", (req, res) => {
   User.create({
     username: req.body.username,
-    password: req.body.password
-  }).then(user =>{
-    res.json(user);
-  })
+    password: req.body.password,
+  }).then((result) => {
+    req.session.userId = result.id;
+    res.json(result);
+  });
 });
 
-//Login
-router.post("/login", async (req, res) =>{
-
+/* ------------------------------- Login Route ------------------------------ */
+router.post("/login", async (req, res) => {
   const result = await User.findOne({
-    username: req.body.username
+    username: req.body.username,
   }).select("+password");
 
+  /* --------------------------------- Bcrypt --------------------------------- */
   const isMatch = await bcrypt.compare(req.body.password, result.password);
 
-  if (isMatch){
+  if (isMatch) {
+    req.session.userId = result.id;
     res.json(result);
   } else {
     res.status(400).json({ message: "Invalid Credentials" });
   }
-
-})
+});
+/* -------------------------------------------------------------------------- */
+module.exports = router;
--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
+require("dotenv").config();
+// app.set('trust proxy,', 1);
+const authRouter = require("./controllers/auth");
+
+const sess = {
+  secret: process.env.SECRET_KEY,
+  cookie: {  }
+};
+
+if (process.env.ENVIRONMENT === "development") {
+  sess.cookie.secure = false;
+}
+
+if (process.env.ENVIRONMENT === "production") {
+  sess.cookie.secure = true;
+}
+
+// console.log(sess.cookie.secure); Cannot make a key-pair value for a key-pair value that doesn't exist
 
 const parser = require("body-parser");
 const cors = require("cors");
@@ -8,5 +27,8 @@ const cors = require("cors");
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 app.use(cors());
+app.use(session(sess));//Creates a session
+//Middleware before routes to activate
+app.use("/auth", authRouter)
 
 app.listen(3001, () => console.log("I'm not serving anything but I'm alive"));
