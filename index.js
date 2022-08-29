51458bbe95ab2e0d0219b96ed3fd8d866adbd376
frostycakes <lamquoc.24@gmail.com>
2022-08-29 19:42:18 -0400

    .get local test

--- index.js
+++ index.js
@@ -64,10 +64,9 @@ app.use("/auth", authRouter);
 app.use("/api", apiRouter);
 //* -------------------------------------------------------------------------- */
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World");
-});
-
+})
 
 
 app.set("port", process.env.PORT || 3001);
