89073f90dd172c97aca4f4e03c3504b773e66774
frostycakes <lamquoc.24@gmail.com>
2022-08-29 18:59:07 -0400

    cors origin without square brackets

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ const cors = require("cors");
 app.set("trust proxy", 1);
 app.use(
   cors({
-    Access-Control-Allow-Origin: "https://positive-place.netlify.app",
+    // Access-Control-Allow-Origin: "https://positive-place.netlify.app",
     credentials: true,
-    origin: ["https://positive-place.netlify.app"],
+    origin: "https://positive-place.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
