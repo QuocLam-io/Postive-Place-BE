b3a39136f811017bd1ec98846fdaed2c02c2dc9a
frostycakes <lamquoc.24@gmail.com>
2022-08-29 20:40:07 -0400

    Add error handler

--- index.js
+++ index.js
@@ -62,6 +62,12 @@ app.use(session(sess)); //Creates a session
 
 app.use("/auth", authRouter);
 app.use("/api", apiRouter);
+
+//* ----------------------------- Error Handling ----------------------------- */
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 //* -------------------------------------------------------------------------- */
 
 app.get("/", (req, res) => {
